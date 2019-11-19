8c58b071c9fbbbba701c42ebd8425785d7cac5ab
Andy Holmes <aholmes@opxglobal.com>
2019-11-19 13:16:43 -0500

    Extracted constants per @felipenevola request

--- src/android/AssetBundle.java
+++ src/android/AssetBundle.java
@@ -191,7 +191,10 @@ class AssetBundle {
             JSONObject runtimeConfig = getRuntimeConfig();
             if (runtimeConfig != null) {
                 try {
-                    rootUrlString = runtimeConfig.getString("ROOT_URL").replace("localhost","10.0.2.2");
+                    const localhost = "localhost";
+                    const emulatorHostLoopbackAddr = "10.0.2.2";
+                    let rootUrlStringOrig = runtimeConfig.getString("ROOT_URL");                   
+                    rootUrlString = rootUrlStringOrig.replace( localhost, emulatorHostLoopbackAddr );
                 } catch (JSONException e) {
                     Log.w(LOG_TAG, "Error reading ROOT_URL from runtime config", e);
                 }
